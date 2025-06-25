fbd0e5e446ee2e632cd7ace3cbd659c57d21358d
Eric Zilvytis <eric@nezser.com>
2025-06-25 06:35:21 +0300

    Fixed FAQ

--- frontend/src/app/faq/page.tsx
+++ frontend/src/app/faq/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
-import Link from 'next/link';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import FAQItem from '../../components/FAQItem';
